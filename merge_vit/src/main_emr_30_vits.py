d01bd18c304a393ad353eec6f5bb1a47841a346a
harveyhuang18 <44834216+harveyhuang18@users.noreply.github.com>
2024-10-04 22:20:08 +0800

    Update main_emr_30_vits.py

--- merge_vit/src/main_emr_30_vits.py
+++ merge_vit/src/main_emr_30_vits.py
@@ -53,67 +53,6 @@ def create_log_dir(path, filename='log.txt'):
     return logger
 
 
-class CUB():
-    def __init__(self, root, is_train=True, data_len=None, transform=None, target_transform=None):
-        self.root = root
-        self.is_train = is_train
-        self.transform = transform
-        self.target_transform = target_transform
-        img_txt_file = open(os.path.join(self.root, 'images.txt'))
-        label_txt_file = open(os.path.join(self.root, 'image_class_labels.txt'))
-        train_val_file = open(os.path.join(self.root, 'train_test_split.txt'))
-        img_name_list = []
-        for line in img_txt_file:
-            img_name_list.append(line[:-1].split(' ')[-1])
-        label_list = []
-        for line in label_txt_file:
-            label_list.append(int(line[:-1].split(' ')[-1]) - 1)
-
-        train_test_list = []
-        for line in train_val_file:
-            train_test_list.append(int(line[:-1].split(' ')[-1]))
-
-        train_file_list = [x for i, x in zip(train_test_list, img_name_list) if i]
-        test_file_list = [x for i, x in zip(train_test_list, img_name_list) if not i]
-
-        train_label_list = [x for i, x in zip(train_test_list, label_list) if i][:data_len]
-        test_label_list = [x for i, x in zip(train_test_list, label_list) if not i][:data_len]
-        if self.is_train:
-            self.train_img = [imageio.imread(os.path.join(self.root, 'images', train_file)) for train_file in
-                              train_file_list[:data_len]]
-            self.train_label = train_label_list
-        if not self.is_train:
-            self.test_img = [imageio.imread(os.path.join(self.root, 'images', test_file)) for test_file in
-                             test_file_list[:data_len]]
-            self.test_label = test_label_list
-
-    # 数据增强
-    def __getitem__(self, index):
-        # 训练集
-        if self.is_train:
-            img, target = self.train_img[index], self.train_label[index]
-        # 测试集
-        else:
-            img, target = self.test_img[index], self.test_label[index]
-
-        if len(img.shape) == 2:
-            img = np.stack([img] * 3, 2)
-        img = Image.fromarray(img, mode='RGB')
-        if self.transform is not None:
-            img = self.transform(img)
-
-        if self.target_transform is not None:
-            target = self.target_transform(target)
-
-        return img, target
-
-    def __len__(self):
-        if self.is_train:
-            return len(self.train_label)
-        else:
-            return len(self.test_label)
-
-
 def eval_single_dataset(image_encoder, dataset, clf, bz, device, ds_name='None'):
     image_encoder.classifier = clf
     model = image_encoder.to(device)
@@ -177,7 +116,6 @@ if __name__ == '__main__':
         # Put your checkpoints Here
         "model_weights/mnist_finetuned",
         "model_weights/cifar10_finetuned_2",
-        "model_weights/vegetables_finetuned",
         # ...
     ]
     exam_datasets = [
